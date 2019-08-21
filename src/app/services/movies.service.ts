c76ef3c75be2ff7f12888145cc9a24dc70b1e562
israel <israel.bejarano.a@gmail.com>
2019-08-21 20:50:33 +0200

    variables globales

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RespuestaMDB } from '../interfaces/interfaces';
+import { environment } from 'src/environments/environment';
+
+const URL = environment.url;
+const apiKey = environment.apiKey;
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +13,26 @@ export class MoviesService {
 
   constructor(private http: HttpClient) { }
 
+  private ejecutarQuery<T>(query: string) {
+    query = URL + query;
+    query += `&apiKey=${apiKey}&language=es&include_image_language=es`;
+    return this.http.get<T>(query);
+  }
+
   getFeature() {
+    const hoy = new Date();
+    const ultimoDia = new Date(hoy.getFullYear(), hoy.getMonth() + 1, 0).getDate();
+    const mes = hoy.getMonth() + 1;
+    let mesString;
+    if (mes < 10) {
+      mesString = '0' + mes;
+    } else {
+      mesString = mes;
+    }
+
+    const inicio = `${hoy.getFullYear()}-${mesString}-01`;
+    const fin = `${hoy.getFullYear()}-${mesString}-${ultimoDia}`;
     // tslint:disable-next-line: max-line-length
-    return this.http.get<RespuestaMDB>(`https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2019-01-01&primary_release_date.lte=2019-01-31&api_key=9f8e099e61c09dca580f0ff522b9d1f9&language=es&include_image_language=es`);
+    return this.ejecutarQuery<RespuestaMDB>(`/discover/movie?primary_release_date.gte=${inicio}&primary_release_date.lte=${fin}`);
   }
 }
