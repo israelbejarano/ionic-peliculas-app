771734ddc49facd52cf85eaf6c288d31ca3b9f6e
israel <israel.bejarano.a@gmail.com>
2019-08-22 22:39:01 +0200

    llamada al detalle component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -5,14 +5,19 @@ import { SlideshowBackdropComponent } from './slideshow-backdrop/slideshow-backd
 import { PipesModule } from '../pipes/pipes.module';
 import { SlideshowPosterComponent } from './slideshow-poster/slideshow-poster.component';
 import { SlideshowParesComponent } from './slideshow-pares/slideshow-pares.component';
+import { DetalleComponent } from './detalle/detalle.component';
 
 
 
 @NgModule({
+  entryComponents:[
+    DetalleComponent
+  ],
   declarations: [
     SlideshowBackdropComponent,
     SlideshowPosterComponent,
-    SlideshowParesComponent
+    SlideshowParesComponent,
+    DetalleComponent
   ],
   imports: [
     CommonModule,
@@ -22,7 +27,8 @@ import { SlideshowParesComponent } from './slideshow-pares/slideshow-pares.compo
   exports: [
     SlideshowBackdropComponent,
     SlideshowPosterComponent,
-    SlideshowParesComponent
+    SlideshowParesComponent,
+    DetalleComponent
   ]
 })
 export class ComponentsModule { }
