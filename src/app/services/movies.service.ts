2b2ea79937c5e2c1daf2b4a4be8147aef642f803
israel <israel.bejarano.a@gmail.com>
2019-08-24 18:33:49 +0200

    pestaña favoritos separados por genero

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { RespuestaMDB, PeliculaDetalle, RespuestaCredits } from '../interfaces/interfaces';
+import { RespuestaMDB, PeliculaDetalle, RespuestaCredits, Genre } from '../interfaces/interfaces';
 import { environment } from 'src/environments/environment';
 
 const URL = environment.url;
@@ -12,6 +12,7 @@ const apiKey = environment.apiKey;
 export class MoviesService {
 
   private popularesPage = 0;
+  generos: Genre[] = [];
 
   constructor(private http: HttpClient) { }
 
@@ -54,5 +55,16 @@ export class MoviesService {
 
   buscarPeliculas(texto: string) {
     return this.ejecutarQuery(`/search/movie?query=${texto}`);
+  }
+
+  cargarGeneros(): Promise<Genre[]> {
+    return new Promise( resolve => {
+      this.ejecutarQuery(`/genre/movie/list?a=1`)
+        .subscribe( (resp: any) => {
+          this.generos = resp.genres;
+          console.log(this.generos);
+          resolve(this.generos);
+        });
+    });
   }
 }
